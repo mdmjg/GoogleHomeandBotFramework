fec3d40d543c353f96409e162be5e2b66048ffcd
Maria del Mar Jaramillo Gutierrez <Mjaramillog@intergrupo.com>
2018-08-03 10:01:32 -0500

    before adding firebase serve

--- functions/index.js
+++ functions/index.js
@@ -9,134 +9,85 @@
 
 const {actionssdk} = require('actions-on-google');
 const functions = require('firebase-functions');
-// exports.myFunction = functions.https.onRequest(app);
+
 const actionsOnGoogleAdapter = require("bot-framework-actions-on-google");
-// const express = require("express");
-// var appExpress = module.exports= express()
 
-//const app = express();
 const app = actionssdk({debug: true});
-///////
-const { DirectLine } = require('botframework-directlinejs');
 
-console.log("arriba")
-// console.log(appExpress.get('port'))
+var express = require('express');
+var router = express.Router();
 
-//const appExp = express();
-
-//appExp.use(actionsOnGoogleAdapter("UXfjuxJwKgg.cwA.m2Q.f7nifUIIXY7T2SmKA7Q2MAQ0vZp6s74UKIoGqRO6izo").router);
-
-
-
-// var directLine = new DirectLine({
-//   secret: "UXfjuxJwKgg.cwA.m2Q.f7nifUIIXY7T2SmKA7Q2MAQ0vZp6s74UKIoGqRO6izo"/* put your Direct Line secret here */,
-//   //token: "UXfjuxJwKgg.cwA.m2Q.f7nifUIIXY7T2SmKA7Q2MAQ0vZp6s74UKIoGqRO6izo"/* or put your Direct Line token here (supply secret OR token, not both) */,
-//   //domain: "https://vendingmachine-b866.azurewebsites.net/api/messages", /* optional: if you are not using the default Direct Line endpoint, e.g. if you are using a region-specific endpoint, put its full URL here */
-//   //webSocket: false /* optional: false if you want to use polling GET to receive messages. Defaults to true (use WebSocket). */,
-//   //pollingInterval: 1000/* optional: set polling interval in milliseconds. Default to 1000 */,
-// });
+var Client = require('node-rest-client').Client;
+ 
+var client = new Client();
+var conversationID = "holis";
+var finalResponse = "Hello hello";
 
 
+var args = {
+    data: { from: "mar", text: "hola", type: "message" },
+    headers: { "Content-Type": "application/json", "Authorization": "Bearer UXfjuxJwKgg.cwA.cYo.EXRcORb0XwBIBkBjvLFMjW8EqcOhukov-9FR_GcT4Fw" }
+};
 
 
+function sendMessage(input, callback) {
+    var args2 = {
+        data: { from: {id: conversationID}, text: input, type: "message" },
+        headers: { "Content-Type": "application/json", "Authorization": "Bearer UXfjuxJwKgg.cwA.cYo.EXRcORb0XwBIBkBjvLFMjW8EqcOhukov-9FR_GcT4Fw" }
+    };
+    client.post(`https://directline.botframework.com/v3/directline/conversations/${conversationID}/activities`, args2, function (data, response) {
+  
+        callback(data);
+    });
+}
 
-  //const directLine = new DirectLine({
-    //secret: "UXfjuxJwKgg.cwA.m2Q.f7nifUIIXY7T2SmKA7Q2MAQ0vZp6s74UKIoGqRO6izo"
-   // conversationId: /* the conversationid you stored from previous conversation */
-  //});
-///////////
-
-// Construct and use router.
-//app.use(actionsOnGoogleAdapter('UXfjuxJwKgg.cwA.m2Q.f7nifUIIXY7T2SmKA7Q2MAQ0vZp6s74UKIoGqRO6izo'));
-
-var BotConnect = require('directline-api-v3');
-var token = "";
-var defaultResponse = "Hey, how are you?";
-
+function getMessage(input, callback) {
+    var args3 = {
+        data: { from: {id: conversationID}, text: input, type: "message" },
+        headers: { "Content-Type": "application/json", "Authorization": "Bearer UXfjuxJwKgg.cwA.cYo.EXRcORb0XwBIBkBjvLFMjW8EqcOhukov-9FR_GcT4Fw" }
+    };
+    client.get(`https://directline.botframework.com/v3/directline/conversations/${conversationID}/activities`, args3, function (data, response) {
+        callback(data);
+    });
+}
 
 
 app.intent('actions.intent.MAIN', (conv) => {
+  client.post("https://directline.botframework.com/v3/directline/conversations", args, function (data, response) {
+  conversationID = data.conversationId;
+});
   conv.ask('Hi!');
 });
 
 
-var token = ""
-var BotConnect = require('directline-api-v3');
-
-BotConnect.getTokenObject('UXfjuxJwKgg.cwA.m2Q.f7nifUIIXY7T2SmKA7Q2MAQ0vZp6s74UKIoGqRO6izo').subscribe(
-    (tokenObject)=>{
-      token = tokenObject.token;
-    },
-    (err)=>console.log(err),
-    ()=>console.log('complete')
-) 
-
-var asyncTask =  new Promise((resolve,reject)=>{
-  let callback = function(err,res) {
-                if(err) console.log(err);
-                else token = token
-    }
-    BotConnect.initConversationStream(token, callback).subscribe(
-        (message)=>{
-                resolve(message);
-        },
-        (err)=>console.log(err),
-        ()=>console.log("complete")
-    )
-})
-
-const asyncTask2 = () => new Promise(
-  resolve => setTimeout(resolve, 1000)
-);
-  
-
-
 app.intent('actions.intent.TEXT', (conv, {input}) => {
-  return asyncTask2()
-    .then(() => conv.ask('I took one second to run'));
+  conv.ask("funciona");
+  var promise1 = new Promise(function(resolve, reject) {
+    // conv.ask("hey gurl before promise");
+    sendMessage(input, function(data){
+        if(data) {
+          conv.ask("hola ma")
+            resolve(data);
+        } else{
+            reject(data);
+        }
+    });
+  });
+    return promise1.then(function(data) {
+        getMessage(input, function(data){
+            finalResponse = data.activities[Object.keys(data.activities).length-1].text;
+            conv.ask("hey gurl");
+        });
+      }, function(err) {
+        console.log(err); // Error: "It broke"
+    });
 });
  
-function initConversation(tokenObject){
-  let callback = function(err,res) {
-            if(err) console.log(err);
-            else token = tokenObject
-  }
-  debugger;
-  BotConnect.initConversationStream(tokenObject, callback).subscribe(
-      (message)=>{
-            lipoTexto(message);
-      },
-      (err)=>console.log(err),
-      ()=>console.log("complete")
-  )
-} 
- 
-
-
-function sendMessageMaria(callback){
-    BotConnect.sendMessage(token,"hello").subscribe(
-                        data=>console.log(data),
-                        err=>console.log(err),
-                        message=>callback(message),
-                        next => console.log(next),
-                        ()=>console.log("complete")
-    )
-  } 
-
-// directLine.activity$
-//  .filter(activity => activity.type === 'message')
-//  .subscribe(
-//      message =>  conv.ask(message)// console.log("received message ", message)
-//  );
-
-
 
 // handle the initialTrigger
 function handleMainIntent(conv, input) {
     conv.ask(`Hi, I'm Do It All. You can ask to do this or that. What would you like to do?`);
   }
-
-
 
 
 // More intent handling if needed
